7110825026b52475a7ff5a54df3128a7dee5aae8
Thomas Heder <thomas.heder@sogeti.com>
2020-12-09 13:09:39 +0100

    9

---
+++ aoc2020/9.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("in.txt");
+            var numbers = lines.Select(f => Convert.ToInt64(f)).ToArray();
+            int preambleSize = 25;
+
+            for (int i = preambleSize; i < numbers.Length; i++)
+            {
+                if (FindSum(numbers, numbers[i], i - preambleSize, i) == false)
+                {
+                    Console.WriteLine(numbers[i]);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static bool FindSum(long[] a, long s, long from, long to)
+        {
+            for (long i = from; i < to; i++)
+            {
+                for (long j = from; j < to; j++)
+                {
+                    if (i != j)
+                    {
+                        if (a[i] != a[j])
+                        {
+                            if (a[i] + a[j] == s)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ aoc2020/9.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("in.txt");
+            var numbers = lines.Select(f => Convert.ToInt64(f)).ToArray();
+
+            long min = long.MaxValue;
+            long max = long.MinValue;
+
+            long at;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Summing from {i}");
+                at = SearchSum(numbers, i, 167829540);
+                if (at > 0)
+                {
+                    Console.WriteLine($"Sum matched from {i} ({numbers[i]}) to {at} ({numbers[at]}) sum {numbers[i] + numbers[at]})");
+
+                    for (int j = i; j < at; j++)
+                    {
+                        min = Math.Min(numbers[j], min);
+                        max = Math.Max(numbers[j], max);
+
+                        Console.WriteLine($"min {min} max {max} sum {min + max}");
+                    }
+
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static long SearchSum(long[] a, long startpos, long searchee)
+        {
+            long sum = 0;
+            for (long i = startpos; i < a.Length; i++)
+            {
+                sum += a[i];
+
+                if (sum == searchee)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
