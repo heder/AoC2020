2faafcf5fc0b2704da5d142f1e5f5f3876f91c98
Thomas Heder <thomas.heder@sogeti.com>
2020-12-11 22:14:02 +0100

    11

---
+++ aoc2020/11.1/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _11._1
+{
+    class Program
+    {
+        static char[,] seats;
+        static char[,] compseats;
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("in.txt").ToArray();
+            seats = new char[lines[0].Length, lines.Length];
+            compseats = new char[lines[0].Length, lines.Length];
+
+
+            int surroundCount;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[0].Length; j++)
+                {
+                    seats[j, i] = lines[i][j];
+                }
+            }
+
+            compseats = seats.Clone() as char[,];
+
+            //DumpMatrix();
+            int changes = 0;
+
+            while (true)
+            {
+                changes = 0;
+                for (int y = 0; y < seats.GetLength(1); y++)
+                {
+                    for (int x = 0; x < seats.GetLength(0); x++)
+                    {
+                        surroundCount = GetSurrounding(x, y);
+
+                        if (seats[x, y] == 'L')
+                        {
+                            if (surroundCount == 0)
+                            {
+                                seats[x, y] = '#';
+                                changes++;
+                            }
+                        }
+
+                        if (seats[x, y] == '#')
+                        {
+                            if (surroundCount >= 4)
+                            {
+                                seats[x, y] = 'L';
+                                changes++;
+                            }
+                        }
+                    }
+                }
+
+                //DumpMatrix();
+                //int n = CountSeats();
+
+                compseats = seats.Clone() as char[,];
+
+                if (changes == 0)
+                {
+                    int n = CountOccupied();
+                    Console.WriteLine(n);
+                    Console.ReadKey();
+                }
+
+                //Console.ReadKey();
+            }
+
+            //Coordinate currentPosition = new Coordinate() { x = 0, y = 0 };
+
+            //int NoOfTrees = 0;
+
+            //while (currentPosition.y < lines.Length)
+            //{
+            //    if (seats[currentPosition.x, currentPosition.y] == '#')
+            //    {
+            //        NoOfTrees++;
+            //    }
+
+            //    currentPosition.x += 3;
+            //    if (currentPosition.x >= seats.GetLength(0))
+            //    {
+            //        currentPosition.x = currentPosition.x - seats.GetLength(0);
+            //    }
+
+
+            //    currentPosition.y += 1;
+            //}
+
+            //Console.WriteLine(NoOfTrees);
+            Console.ReadKey();
+        }
+
+
+        static void DumpMatrix()
+        {
+            for (int y = 0; y < seats.GetLength(1); y++)
+            {
+                for (int x = 0; x < seats.GetLength(0); x++)
+                {
+                    Console.Write(seats[x, y]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+            Console.Write(Environment.NewLine);
+        }
+
+        static int CountOccupied()
+        {
+            int n = 0;
+            for (int y = 0; y < seats.GetLength(1); y++)
+            {
+                for (int x = 0; x < seats.GetLength(0); x++)
+                {
+                    if (seats[x, y] == '#')
+                    {
+                        n++;
+                    }
+                }
+            }
+
+            return n;
+        }
+
+
+        static int GetSurrounding(int x, int y)
+        {
+            int c = 0;
+
+            if (x >= 1 && y >= 1 && compseats[x - 1, y - 1] == '#') c++; // TOPLEFT
+            if (y >= 1 && compseats[x, y - 1] == '#') c++; // TOP
+            if (y >= 1 && x < compseats.GetUpperBound(0) && compseats[x + 1, y - 1] == '#') c++; // TOPRIGHT
+
+            if (x >= 1 && compseats[x -1 , y] == '#') c++; // LEFT
+            if (x < compseats.GetUpperBound(0) && compseats[x + 1, y] == '#') c++; // RIGHT
+
+            if (y < compseats.GetUpperBound(1) && x >= 1 && compseats[x - 1, y + 1] == '#') c++; // BOTTOMLEFT
+            if (y < compseats.GetUpperBound(1) && compseats[x, y + 1] == '#') c++; // BOTTOM
+            if (y < compseats.GetUpperBound(1) && x < compseats.GetUpperBound(0) &&  compseats[x + 1, y + 1] == '#') c++; // BOTTOMRIGHT
+
+
+
+            return c;
+        }
+
+
+        static int GetSafeX(int x)
+        {
+            if (x < 0) return 0;
+            else if (x > compseats.GetUpperBound(0)) return compseats.GetUpperBound(0);
+            else return x;
+        }
+
+        static int GetSafeY(int y)
+        {
+            if (y < 0) return 0;
+            else if (y > compseats.GetUpperBound(1)) return compseats.GetUpperBound(1);
+            else return y;
+        }
+    }
+}
---
+++ aoc2020/11.2/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _11._2
+{
+    class Program
+    {
+        static char[,] seats;
+        static char[,] compseats;
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("in.txt").ToArray();
+            seats = new char[lines[0].Length, lines.Length];
+            compseats = new char[lines[0].Length, lines.Length];
+
+
+            int surroundCount;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[0].Length; j++)
+                {
+                    seats[j, i] = lines[i][j];
+                }
+            }
+
+            compseats = seats.Clone() as char[,];
+
+            // DumpMatrix();
+            int changes = 0;
+
+            while (true)
+            {
+                changes = 0;
+                for (int y = 0; y < seats.GetLength(1); y++)
+                {
+                    for (int x = 0; x < seats.GetLength(0); x++)
+                    {
+                        surroundCount = GetVisible(x, y);
+
+                        if (seats[x, y] == 'L')
+                        {
+                            if (surroundCount == 0)
+                            {
+                                seats[x, y] = '#';
+                                changes++;
+                            }
+                        }
+
+                        if (seats[x, y] == '#')
+                        {
+                            if (surroundCount >= 5)
+                            {
+                                seats[x, y] = 'L';
+                                changes++;
+                            }
+                        }
+                    }
+                }
+
+                compseats = seats.Clone() as char[,];
+
+                if (changes == 0)
+                {
+                    int n = CountOccupied();
+                    Console.WriteLine(n);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+
+        static void DumpMatrix()
+        {
+            for (int y = 0; y < seats.GetLength(1); y++)
+            {
+                for (int x = 0; x < seats.GetLength(0); x++)
+                {
+                    Console.Write(seats[x, y]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+            Console.Write(Environment.NewLine);
+        }
+
+        static int CountOccupied()
+        {
+            int n = 0;
+            foreach (var item in seats)
+            {
+                if (item == '#') n++;
+            }
+
+            return n;
+        }
+
+
+        static int GetVisible(int x, int y)
+        {
+            int c = 0;
+            int x1;
+            int y1;
+
+            // UP
+            x1 = x; y1 = y;
+            while (y1 > 0)
+            {
+                y1--;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // DOWN
+            x1 = x; y1 = y;
+            while (y1 < compseats.GetUpperBound(1))
+            {
+                y1++;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // LEFT
+            x1 = x; y1 = y;
+            while (x1 > 0)
+            {
+                x1--;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // RIGHT
+            x1 = x; y1 = y;
+            while (x1 < compseats.GetUpperBound(0))
+            {
+                x1++;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // UPLEFT
+            x1 = x; y1 = y;
+            while (x1 > 0 && y1 > 0)
+            {
+                x1--;
+                y1--;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // UPRIGHT
+            x1 = x; y1 = y;
+            while (x1 < compseats.GetUpperBound(0) && y1 > 0)
+            {
+                x1++;
+                y1--;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // DOWNRIGHT
+            x1 = x; y1 = y;
+            while (x1 < compseats.GetUpperBound(0) && y1 < compseats.GetUpperBound(1))
+            {
+                x1++;
+                y1++;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            // DOWNLEFT
+            x1 = x; y1 = y;
+            while (x1 > 0 && y1 < compseats.GetUpperBound(1))
+            {
+                x1--;
+                y1++;
+                if (compseats[x1, y1] == 'L') { break; }
+                if (compseats[x1, y1] == '#') { c++; break; }
+            }
+
+            return c;
+        }
+    }
+}
