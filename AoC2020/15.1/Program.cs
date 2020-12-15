78d02226f908c988277b60f91ad9ac1d2bb6378b
Thomas Heder <thomas.heder@gmail.com>
2020-12-15 21:15:26 +0100

    15

---
+++ AoC2020/15.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _15._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = File.ReadLines("in.txt").First().Split(",").Select(f => Convert.ToInt32(f)).ToArray();
+            int[] numbers = new int[2020];
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                numbers[j] = -1;
+            }
+
+            int i;
+            for (i = 0; i < start.Length; i++)
+            {
+                numbers[i] = start[i];
+            }
+
+            bool isLastUnique = true;
+            int lastNum = numbers[i - 1];
+            while (i < 2020)
+            {
+                if (isLastUnique == true)
+                {
+                    if (numbers.Contains(0))
+                    {
+                        isLastUnique = false;
+                    }
+                    else
+                    {
+                        isLastUnique = true;
+                    }
+
+                    numbers[i] = lastNum = 0;
+                }
+                else
+                {
+                    var last2 = numbers
+                        .Select((v, i) => new { v, i })
+                        .Where(f => f.v == lastNum)
+                        .TakeLast(2)
+                        .ToArray();
+
+                    var diff = last2[1].i - last2[0].i;
+
+                    if (numbers.Contains(diff))
+                    {
+                        isLastUnique = false;
+                    }
+                    else
+                    {
+                        isLastUnique = true;
+                    }
+                    
+                    numbers[i] = lastNum = diff;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine(numbers[2019]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AoC2020/15.2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace b15
+{
+    class Positions
+    {
+        public long[] q = new long[2];
+
+        public Positions(long v)
+        {
+            q[0] = v;
+            q[1] = -1;
+        }
+
+        public void Enqueue(long v)
+        {
+            q[1] = q[0];
+            q[0] = v;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = File.ReadLines("in.txt").First().Split(",").Select(f => Convert.ToInt64(f)).ToArray();
+            long iterations = 30000000;
+
+            Dictionary<long, Positions> numbers = new Dictionary<long, Positions>();
+
+            int i;
+            for (i = 0; i < start.Length; i++)
+            {
+                numbers.Add(start[i], new Positions(i));
+            }
+
+            long diff = -1;
+            bool isLastUnique = true;
+            long lastNum = numbers.Last().Key;
+
+            while (i < iterations)
+            {
+                if (isLastUnique == true)
+                {
+                    if (numbers.ContainsKey(0))
+                    {
+                        isLastUnique = false;
+                        numbers[0].Enqueue(i);
+                    }
+                    else
+                    {
+                        isLastUnique = true;
+                        numbers.Add(0, new Positions(i));
+                    }
+
+                    lastNum = 0;
+                    
+                }
+                else
+                {
+                    diff = numbers[lastNum].q[0] - numbers[lastNum].q[1];
+
+                    if (numbers.ContainsKey(diff))
+                    {
+                        isLastUnique = false;
+                        numbers[diff].Enqueue(i);
+                    }
+                    else
+                    {
+                        isLastUnique = true;
+                        numbers.Add(diff, new Positions(i));
+                    }
+
+                    lastNum = diff;
+                }
+
+                i++;
+
+                if (i % 1000000 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine(lastNum);
+            Console.ReadKey();
+        }
+    }
+}
